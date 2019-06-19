e3b281955ef5d025ddf20b66a2953d1086451033
marcelocs19 <marcelo.home19@gmail.com>
2019-06-19 16:18:08 -0300

    Atualização

--- src/main/java/hello/CustomerController.java
+++ src/main/java/hello/CustomerController.java
@@ -1,12 +1,13 @@
 package hello;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class CustomerController {
@@ -22,10 +23,25 @@ public class CustomerController {
 	public String listAllCustomers(Model model) {
 		List<Customer> lista = customers.findAll();
 		
-		Collections.sort(lista, Comparator.comparing(Customer:: getFirstName));
+		//Collections.sort(lista, Comparator.comparing(Customer:: getFirstName));
 		
 		model.addAttribute("cs", lista);
 		return "customersList";
+	}
+	
+	@GetMapping("/customers/{id}")
+//	public ModelAndView listAllCustomers(@PathVariable long id, Model model) {
+//		ModelAndView mav = new ModelAndView("customerDetails");
+//		mav.addObject(this.customers.findById(id));
+//		return mav;
+	public String listAllCustomers(@PathVariable long id, Model model) {
+		Optional<Customer> c = customers.findById(id);	
+		if(c.isPresent()) {
+			model.addAttribute("customer",c.get());
+			return "customerDetails";
+		}else {
+			return "redirect:/erro";
+		}
 	}
 
 
